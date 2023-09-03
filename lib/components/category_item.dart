9e4d7898c4675ce0ee9e0b342c761539d4df348c
Hazem12Bakr <hazem.abobakr.200320@btu.edu.eg>
2023-09-03 21:33:36 +0300

    adding navigation and other screens

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -5,25 +5,29 @@ import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class Category extends StatelessWidget {
-   Category({this.text,this.color});
+   Category({this.text,this.color,this.onTap});
    String? text;
    Color? color;
+   VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-              alignment: Alignment.centerLeft,
-              padding:const EdgeInsets.only(left: 16),
-              height: 56,
-              width: double.infinity,
-              color: color,
-              child: Text(
-                text!,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+                alignment: Alignment.centerLeft,
+                padding:const EdgeInsets.only(left: 16),
+                height: 56,
+                width: double.infinity,
+                color: color,
+                child: Text(
+                  text!,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
                 ),
               ),
-            );
+    );
   }
 }
---
+++ lib/screens/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorsM extends StatelessWidget {
+  const ColorsM({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
